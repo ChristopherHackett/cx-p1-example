2ab1ab51bd8e4b5d33c10be7a2809185257c9810
Christopher Hackett <ChristopherHackett@users.noreply.github.com>
2016-07-25 13:25:27 +0100

    init with example code

---
+++ App1/js/script.js
@@ -0,0 +1,3 @@
+﻿var cX = cX || {}; cX.callQueue = cX.callQueue || [];
+cX.callQueue.push(['setSiteId', 'xxxxx']);
+cX.callQueue.push(['sendPageViewEvent', {'location': 'http://xbox.gfinity.net/default.html'}]);
